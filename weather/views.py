b329d11d37709ec435ac14addcf32b0fcbe3b39c
Lucas Paifer <paiferlucas2212@gmail.com>
2025-05-18 22:40:19 -0300

    first version

---
+++ weather/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+import requests
+
+def index(request):
+    city = request.GET.get('city', 'Sorocaba')
+    api_key = '54e719358f71910be672001072a0806b'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}'
+    
+    weather_data = {}
+    error_message = None
+    
+    try:
+        response = requests.get(url).json()
+
+        if response.get('cod') !=200:
+            raise ValueError(f"City '{city}' not found.")
+        
+        weather_data = {
+            'city': city,
+            'temperature': response['main']['temp'],
+            'description': response['weather'][0]['description'],
+            'humidity': response['main']['humidity'],
+            'wind_speed': response['wind']['speed']
+        }
+    except ValueError as e:
+        error_message = str(e)
+
+    return render(request, 'weather/index.html', {'weather_data': weather_data, 'error_message': error_message})
